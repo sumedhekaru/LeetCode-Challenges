9a8a2e870ee4317c1867a562a085aab431d62daf
sumedhekaru <31940881+sumedhekaru@users.noreply.github.com>
2019-08-18 21:33:47 -0500

    Update add-two-numbers.py

--- add-two-numbers.py
+++ add-two-numbers.py
@@ -8,6 +8,22 @@ Created on Sun Aug 18 12:35:48 2019
 # Since the ListNode class is given, and not implmented here, the code cannot
 # test outside LeetCode enviornoment, not unless you wrote your own ListNode class.
 
+"""
+Problem: You are given two non-empty linked lists representing two non-negative 
+integers. The digits are stored in reverse order and each of their nodes contain 
+a single digit. Add the two numbers and return it as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the 
+number 0 itself.
+
+Example:
+
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+Explanation: 342 + 465 = 807.
+
+"""
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
