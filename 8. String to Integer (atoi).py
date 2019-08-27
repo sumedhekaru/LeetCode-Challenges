c8b8f600d56185154aaff93f067216639c609619
sumedhekaru <31940881+sumedhekaru@users.noreply.github.com>
2019-08-26 23:36:51 -0500

    Update 8. String to Integer (atoi).py

--- 8. String to Integer (atoi).py
+++ 8. String to Integer (atoi).py
@@ -7,69 +7,58 @@ Created on Mon Aug 26 19:37:58 2019
 
 class Solution:
     def myAtoi(self, str):
+
+      # def myAtoi(self, str):
         
+        str = str.strip()
         L = len(str)
-    
-        start_index = float('inf')
         
-        for c in '0123456789':
-            temp = str.find(c)
-            
-            if temp != -1 and temp < start_index:
-                start_index = temp
-        
-        # fix the negative sign
-        #if start_index != 0 and str[start_index -1] == "-":
-        #    sign = -1
-        #else:
-        #    sign = 1
-        
-        #print(start_index)
-        # return zero for leading letters
-        leading = str[0:start_index].strip()
-        #print(leading)
-        
-        if leading == '-':
-            sign = -1
-        elif leading == '+':
-            sign = 1
-        else:
+        if L == 0:
             return 0
+        
+        if L == 1 and not str.isnumeric():
+            return 0
+        
+        sign = 1
+        
+        # first character should be numeric if not return 0
+        if str[0] == '-':
+            sign = -1
+            str = str[1:]
+        elif str[0] == '+':
+            sign = 1
+            str = str[1:]
+        elif not str[0].isnumeric():
+            return 0
+        
+        num = ''
+        
+        #print(str)
+        
+        for i, c in enumerate(str):
+            #print(c)
+            if i > 10:
+                    break
                 
+            if c.isnumeric():  
+                num = num + c
+            else:
+                break
         
-        #print(start_index)
+        try:
+            num = int(num)*sign
+        except ValueError:
+            return 0
+        #print(num)
         
-        if start_index + 10 > L:
-            last_index = L
+        if num > 2**31-1:
+            return 2**31-1
+        elif num < -2**31:
+            return -2**31
         else:
-            last_index = L + 10
-            
-        # Fix the last index
-        found = False
-        counter = start_index
-        
-        while not found:
-            try:
-                x = int(str[counter])
-                #print(x)
-            except:
-                last_index = counter
-                found = True
-            
-            counter += 1
-            
-        
-        #print(last_index)
-        
-        number =  int(str[start_index:last_index])
-        
-        if number > 2**31:
-            number = 2147483648
-    
-        return number*sign
-    
+            return num
+ 
+
             
             
-            
-            
-print(Solution().myAtoi('-25 758asdfsd'))
+print(Solution().myAtoi('+-2'))
